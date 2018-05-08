62b373c7d860c4a66be213de516c50a437576bdf
Sakamotto <csanterio@gmail.com>
2018-05-08 10:49:43 -0300

    Alterção no diagrama de projeto

--- loja-vitual-backend/api/models/Cliente.js
+++ loja-vitual-backend/api/models/Cliente.js
@@ -11,9 +11,12 @@ module.exports = {
     // PRIMITIVES
 
     nome: {type: 'string', required: true},
+    sobrenome: {type: 'string', required: true},
     email: {type: 'string', required: true, unique: true},
     senha: {type: 'string', required: true, encrypt: true},
     dataNascimento: {type: 'string'},
+    dataCadastro: {type: 'string', defaultsTo: new Date().toISOString()},
+    ativo: {type: 'boolean', defaultsTo: true},
     cpf: {type: 'string'},
     telefone: {type: 'string'},
 
